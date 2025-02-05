431d559ae9b4589748f00644d5a9724561e15ef8
SunMaoMao <smm1998121>
2025-02-05 22:04:00 +0800

    项目构建

---
+++ gilimail-ware/src/main/java/com/itguigu/gilimailware/GilimailWareApplication.java
@@ -0,0 +1,13 @@
+package com.itguigu.gilimailware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilimailWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GilimailWareApplication.class, args);
+    }
+
+}
---
+++ gulimail-order/src/main/java/com/itguigu/gulimailorder/GulimailOrderApplication.java
@@ -0,0 +1,13 @@
+package com.itguigu.gulimailorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimailOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimailOrderApplication.class, args);
+    }
+
+}
